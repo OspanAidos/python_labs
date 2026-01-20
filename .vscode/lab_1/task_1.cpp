76ae7c3e2be03ae9b40530feb90a3a67eac5bd70
OspanAidos <ospanaidos.00@gmail.com>
2026-01-20 15:39:36 +0500

    123

--- .vscode/lab_1/task_1.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <string>
-using namespace std;
-int main(){
-    setlocale(LC_ALL, "RU");
-    float n;
-    cin >>n;
-    cout<<n*n*n;
-}
--- .vscode/lab_1/task_6.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    int a;
-    cin>>a;
-    cout<<a%10*1000+a/10%10*100+a/100%10*10+a/1000<<endl;
-    return 0;
-}
